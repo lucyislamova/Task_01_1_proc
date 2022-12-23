8a0d00acf8cd78607e383280308572f8add4b71a
Lucy Islamova <islamoval@inlinegroup-c.ru>
2022-12-23 15:21:20 +0300

    Без ООП

--- src/ru/inlinec/edu/Main.java
+++ src/ru/inlinec/edu/Main.java
@@ -4,47 +4,46 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner;
-    static float sideA;
-    static float sideB;
-    static float sideC;
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        try {
-            sideA = readSideLen("a");
-            sideB = readSideLen("b");
-            sideC = readSideLen("c");
+        float sideA = readSideLen("a");
+        float sideB = readSideLen("b");
+        float sideC = readSideLen("c");
 
-            checkTriangle();
+        checkTriangle(sideA, sideB, sideC);
 
-            printHeights();
-        } catch (IllegalAccessException exc) {
-            System.out.println(exc.getMessage());
-        }
+        float heightA = getHeight(sideA, sideA, sideB, sideC);
+        float heightB = getHeight(sideB, sideA, sideB, sideC);
+        float heightC = getHeight(sideC, sideA, sideB, sideC);
+
+        System.out.println("Высоты сторон треугольника:");
+        printHeight("A", heightA);
+        printHeight("B", heightB);
+        printHeight("C", heightC);
     }
-    private static float readSideLen( String sideName ){
+
+    private static float readSideLen(String sideName) {
         System.out.printf("Сторона %s: ", sideName);
-        return  scanner.nextFloat();
+        float side = scanner.nextFloat();
+        if (side <= 0)
+            throw new IllegalArgumentException("Введите корректные данные: сторона треугольника должна быть больше 0");
+        return side;
     }
 
-    public static void checkTriangle() throws IllegalAccessException {
-        if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
-            throw new IllegalAccessException("Введите корректные данные: стороны треугольника должны быть больше 0");
+    private static void checkTriangle(float sideA, float sideB, float sideC) {
+        if (sideA > (sideB + sideC) || sideB > (sideB + sideC) || sideC > (sideA + sideB)) {
+            throw new IllegalArgumentException("Введите корректные данные: сторона треугольника не может быть больше суммы 2х других сторон");
         }
-        if (sideA > (sideB + sideC) || sideB > (sideB + sideC) || sideC > (sideA + sideB)){
-
-            throw new IllegalAccessException("Введите корректные данные: сторона треугольника не может быть больше суммы 2х других сторон");
-        }
-    }
-    private static float getHeight(float side){
-        float semiP = (sideA + sideB + sideC)/2; //полупериметр
-        return 2/side * (float)Math.sqrt((semiP - sideA) * (semiP - sideB) * (semiP - sideC));
     }
 
-    public static void printHeights(){
-        System.out.printf("Высота стороны A = %.3f\n", getHeight(sideA));
-        System.out.printf("Высота стороны B = %.3f\n", getHeight(sideB));
-        System.out.printf("Высота стороны C = %.3f\n", getHeight(sideC));
+    private static void printHeight(String side, float height) {
+        System.out.printf("%s = %.3f\n", side, height);
+    }
+
+    private static float getHeight(float sideCurrent, float sideA, float sideB, float sideC) {
+        float semiP = (sideA + sideB + sideC) / 2;
+        return 2 / sideCurrent * (float) Math.sqrt((semiP - sideA) * (semiP - sideB) * (semiP - sideC));
     }
 
 }
